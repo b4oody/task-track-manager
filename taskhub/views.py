8049d6beb29cdd6a90de715f38603072d2801418
VR <vladrymarchuk@gmail.com>
2024-11-14 20:42:57 +0200

    feat: Add profile view to fetch worker details with tasks and project counts
      - Implemented `get_profile` view to display worker profile information.
      - Prefetch related `teams` and `tasks` for the worker to optimize query performance.
      - Aggregated tasks data to include counts of active and finished tasks.
      - Added distinct count of finished projects across teams associated with the worker.
      - Passed worker profile, task counts, and project count data to the `profile.html` template.

--- taskhub/views.py
+++ taskhub/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
 from taskhub.form import RegistrationForm
-from taskhub.models import Position
+from taskhub.models import Worker, Project, Position
 
 
 def get_index_page(request: HttpRequest) -> HttpResponse:
@@ -26,3 +28,23 @@ def sign_up(request: HttpRequest) -> HttpResponse:
         request,
         "registration/register.html",
         {"form": form, "positions": positions})
+
+
+@login_required
+def get_profile(request: HttpRequest) -> HttpResponse:
+    worker = Worker.objects.prefetch_related("teams", "tasks").get(pk=request.user.id)
+    tasks = worker.tasks.aggregate(
+        active_tasks=Count("id", filter=Q(is_completed=False)),
+        finished_tasks=Count("id", filter=Q(is_completed=True)),
+    )
+    finished_projects_count = Project.objects.filter(
+        team__in=worker.teams.all()
+    ).distinct().count()
+    context = {
+        "worker": worker,
+        "active_tasks": tasks["active_tasks"],
+        "finished_projects": finished_projects_count,
+        "finished_tasks": tasks["finished_tasks"],
+    }
+
+    return render(request, "profile/profile.html", context=context)
