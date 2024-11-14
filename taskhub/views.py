0ad2350ed9e400377ead507709b99b8653fa698c
VR <vladrymarchuk@gmail.com>
2024-11-14 18:17:29 +0200

    feat: add the function 'get_index_page' for rendering index page

--- taskhub/views.py
+++ taskhub/views.py
@@ -1,3 +1,7 @@
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_index_page(request: HttpRequest) -> HttpResponse:
+    return render(request, "index/index.html")
