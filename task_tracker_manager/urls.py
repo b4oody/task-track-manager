7840915b6b46bdbe01a28944b9d8ce88b4292d0f
VR <vladrymarchuk@gmail.com>
2024-11-14 18:20:43 +0200

    feat: add path 'accounts/' for include built-in auth

--- task_tracker_manager/urls.py
+++ task_tracker_manager/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taskhub.urls", namespace="taskhub"),),
+    path("accounts/", include("django.contrib.auth.urls"),),
 
 ] + debug_toolbar_urls()
