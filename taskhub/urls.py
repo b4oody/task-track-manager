be82fee971dbf33f778e076a60d92f73c0d5a3cb
VR <vladrymarchuk@gmail.com>
2024-11-14 20:45:03 +0200

    feat: add path the 'profile/' for get_profile function

--- taskhub/urls.py
+++ taskhub/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from taskhub.views import (
     get_index_page,
+    get_profile,
     sign_up
 )
 
 urlpatterns = [
     path("", get_index_page, name="index"),
+    path("profile/", get_profile, name="profile"),
     path("register/", sign_up, name="register"),
 ]
 
