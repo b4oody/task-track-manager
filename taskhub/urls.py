7f86c6de57335e30702f4157e009a0970435562f
VR <vladrymarchuk@gmail.com>
2024-11-14 18:30:38 +0200

    feat: add path 'register/' for sign_up func

--- taskhub/urls.py
+++ taskhub/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from taskhub.views import (
     get_index_page,
+    sign_up
 )
 
 urlpatterns = [
     path("", get_index_page, name="index"),
+    path("register/", sign_up, name="register"),
 ]
 
 app_name = 'taskhub'
