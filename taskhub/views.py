4c65de8e4728c499755651b8c2e314c7d9010d45
VR <vladrymarchuk@gmail.com>
2024-11-14 18:28:38 +0200

    feat: add sign_up function for user registration in taskhub/view.py

--- taskhub/views.py
+++ taskhub/views.py
@@ -1,7 +1,28 @@
-
+from django.contrib.auth import login
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from taskhub.form import RegistrationForm
+from taskhub.models import Position
 
 
 def get_index_page(request: HttpRequest) -> HttpResponse:
     return render(request, "index/index.html")
+
+
+def sign_up(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            worker = form.save(commit=False)
+            worker.position = form.cleaned_data["position"]
+            worker.save()
+            login(request, worker)
+            return redirect("taskhub:profile")
+    else:
+        form = RegistrationForm()
+    positions = Position.objects.all()
+    return render(
+        request,
+        "registration/register.html",
+        {"form": form, "positions": positions})
