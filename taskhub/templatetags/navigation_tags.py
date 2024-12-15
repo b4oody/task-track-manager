3a0aeb4e02d3592f7d9248871d6a603631c8245a
VR <vladrymarchuk@gmail.com>
2024-12-15 03:47:01 +0200

    chore: remove unnecessary files from project structure

--- taskhub/templatetags/navigation_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def get_referer(context, default="/"):
-    request = context.get("request", None)
-    return request.META.get("HTTP_REFERER", default)
